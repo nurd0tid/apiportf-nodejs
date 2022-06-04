b01b3a474f04dd39a3dc1c357664b75a9381ed1b
nurd0tid <mnur29054@gmail.com>
2022-06-04 21:39:22 +0700

    Insert with Upload Image Package multer

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path')
 const bodyParser = require('body-parser');
 const identityRoutes = require('./utils/routes/identityRoutes');
 const app = express();
@@ -7,9 +8,24 @@ const PORT = process.env.PORT || 5000;
 // set body parser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.set('views',path.join(__dirname,'views'));
+app.set('view engine', 'ejs');
+app.use('/assets',express.static(__dirname + '/public/assets'));
+
 
 // set routing
 app.use('/api/identity', identityRoutes);
+
+// group pages identity
+app.get('/dashboard', function(req, res) {
+    res.render('pages/dashboard/v_index');
+});
+
+// group pages identity
+app.get('/identity', function(req, res) {
+    res.render('pages/identity/v_index');
+});
+
 
 // buat server nya
 app.listen(PORT, () => console.log(`Server running at port: ${PORT}`));
--- utils/controllers/Identity.js
+++ utils/controllers/Identity.js
@@ -4,15 +4,41 @@ const {
     updateIdentity,
     deleteIdentity
 } = require('../models/M_identity');
+const multer = require('multer');
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'public/img/');
+    },
+    filename: (req, file, cb) => {
+        cb(null, Date.now()+'-'+file.originalname);
+    }
+});
+
+const upload = multer({ storage });
+
 
 // create Identity
 exports.createData = (req, res, next) => {
-  // variable data and query sql
-  const data = { ...req.body };
-  const querySql = 'INSERT INTO identity SET ?';
+    upload.single('photo')(req, res, () => {
+    const data = { 
+      photo: req.file === undefined ? "" : req.file.filename,
+      name: req.body.name,
+      about: req.body.about,
+      company: req.body.company,
+      job: req.body.job,
+      country: req.body.country,
+      address: req.body.address,
+      phone: req.body.phone,
+      email: req.body.email,
+      instagram: req.body.instagram,
+      linkedin: req.body.linkedin,
+      github: req.body.github
+     };
+    const querySql = 'INSERT INTO identity SET ?';
   
-  // import to model
+    // import to model
     insertIdentity(res, querySql, data);
+    });
 }
 
 exports.readData = (req, res) => {
