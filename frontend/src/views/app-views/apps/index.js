dc166f3b9684a2678ca2cc694e5f0408400d0309
nurd0tid <mnur29054@gmail.com>
2022-06-24 10:02:54 +0700

    Create Sub Folder Master for menu master

--- frontend/src/views/app-views/apps/index.js
+++ frontend/src/views/app-views/apps/index.js
@@ -5,31 +5,52 @@ import Loading from 'components/shared-components/Loading';
 const Apps = ({ match }) => (
   <Suspense fallback={<Loading cover="content"/>}>
     <Switch>
-      <Route path={`${match.url}/identitas`} component={lazy(() => import(`./identitas`))} />
-      <Route path={`${match.url}/kurikulum`} component={lazy(() => import(`./kurikulum`))} />
-      <Route path={`${match.url}/kurikulum-add`} component={lazy(() => import(`./kurikulum/add`))} />
-      <Route path={`${match.url}/kurikulum-edit/:id`} component={lazy(() => import(`./kurikulum/edit`))} />
-      <Route path={`${match.url}/tahun-akademik`} component={lazy(() => import(`./akademik`))} />
-      <Route path={`${match.url}/tahun-akademik-add`} component={lazy(() => import(`./akademik/add`))} />
-      <Route path={`${match.url}/tahun-akademik-edit/:id`} component={lazy(() => import(`./akademik/edit`))} />
-      <Route path={`${match.url}/gedung`} component={lazy(() => import(`./gedung`))} />
-      <Route path={`${match.url}/gedung-add`} component={lazy(() => import(`./gedung/add`))} />
-      <Route path={`${match.url}/gedung-edit/:id`} component={lazy(() => import(`./gedung/edit`))} />
-      <Route path={`${match.url}/ruangan`} component={lazy(() => import(`./ruangan`))} />
-      <Route path={`${match.url}/ruangan-add`} component={lazy(() => import(`./ruangan/add`))} />
-      <Route path={`${match.url}/ruangan-edit/:id`} component={lazy(() => import(`./ruangan/edit`))} />
-      <Route path={`${match.url}/golongan`} component={lazy(() => import(`./golongan`))} />
-      <Route path={`${match.url}/golongan-add`} component={lazy(() => import(`./golongan/add`))} />
-      <Route path={`${match.url}/golongan-edit/:id`} component={lazy(() => import(`./golongan/edit`))} />
-      <Route path={`${match.url}/ptk`} component={lazy(() => import(`./ptk`))} />
-      <Route path={`${match.url}/ptk-add`} component={lazy(() => import(`./ptk/add`))} />
-      <Route path={`${match.url}/ptk-edit/:id`} component={lazy(() => import(`./ptk/edit`))} />
-      <Route path={`${match.url}/jurusan`} component={lazy(() => import(`./jurusan`))} />
-      <Route path={`${match.url}/jurusan-add`} component={lazy(() => import(`./jurusan/add`))} />
-      <Route path={`${match.url}/jurusan-detail/:id`} component={lazy(() => import(`./jurusan/detail`))} />
-      <Route path={`${match.url}/kepegawaian`} component={lazy(() => import(`./kepegawaian`))} />
-      <Route path={`${match.url}/kepegawaian-add`} component={lazy(() => import(`./kepegawaian/add`))} />
-      <Route path={`${match.url}/kepegawaian-edit/:id`} component={lazy(() => import(`./kepegawaian/edit`))} />
+      <Route path={`${match.url}/identitas`} component={lazy(() => import(`./master/identitas`))} />
+
+       {/* Function view Kurikulum. */}
+      <Route path={`${match.url}/kurikulum`} component={lazy(() => import(`./master/kurikulum`))} />
+      <Route path={`${match.url}/kurikulum-add`} component={lazy(() => import(`./master/kurikulum/add`))} />
+      <Route path={`${match.url}/kurikulum-edit/:id`} component={lazy(() => import(`./master/kurikulum/edit`))} />
+
+       {/* Function view Tahun Akademik. */}
+      <Route path={`${match.url}/tahun-akademik`} component={lazy(() => import(`./master/akademik`))} />
+      <Route path={`${match.url}/tahun-akademik-add`} component={lazy(() => import(`./master/akademik/add`))} />
+      <Route path={`${match.url}/tahun-akademik-edit/:id`} component={lazy(() => import(`./master/akademik/edit`))} />
+
+       {/* Function view Gedung. */}
+      <Route path={`${match.url}/gedung`} component={lazy(() => import(`./master/gedung`))} />
+      <Route path={`${match.url}/gedung-add`} component={lazy(() => import(`./master/gedung/add`))} />
+      <Route path={`${match.url}/gedung-edit/:id`} component={lazy(() => import(`./master/gedung/edit`))} />
+
+       {/* Function view Ruangan. */}
+      <Route path={`${match.url}/ruangan`} component={lazy(() => import(`./master/ruangan`))} />
+      <Route path={`${match.url}/ruangan-add`} component={lazy(() => import(`./master/ruangan/add`))} />
+      <Route path={`${match.url}/ruangan-edit/:id`} component={lazy(() => import(`./master/ruangan/edit`))} />
+
+      {/* Function view Golongan. */}
+      <Route path={`${match.url}/golongan`} component={lazy(() => import(`./master/golongan`))} />
+      <Route path={`${match.url}/golongan-add`} component={lazy(() => import(`./master/golongan/add`))} />
+      <Route path={`${match.url}/golongan-edit/:id`} component={lazy(() => import(`./master/golongan/edit`))} />
+
+       {/* Function view PTK. */}
+      <Route path={`${match.url}/ptk`} component={lazy(() => import(`./master/ptk`))} />
+      <Route path={`${match.url}/ptk-add`} component={lazy(() => import(`./master/ptk/add`))} />
+      <Route path={`${match.url}/ptk-edit/:id`} component={lazy(() => import(`./master/ptk/edit`))} />
+
+       {/* Function view Jurusan. */}
+      <Route path={`${match.url}/jurusan`} component={lazy(() => import(`./master/jurusan`))} />
+      <Route path={`${match.url}/jurusan-add`} component={lazy(() => import(`./master/jurusan/add`))} />
+      <Route path={`${match.url}/jurusan-detail/:id`} component={lazy(() => import(`./master/jurusan/detail`))} />
+
+      {/* Function view Kelas. */}
+      <Route path={`${match.url}/kelas`} component={lazy(() => import(`./master/kelas`))} />
+      <Route path={`${match.url}/kelas-add`} component={lazy(() => import(`./master/kelas/add`))} />
+      <Route path={`${match.url}/kelas-edit/:id`} component={lazy(() => import(`./master/kelas/edit`))} />
+
+       {/* Function view Kepegawaian. */}
+      <Route path={`${match.url}/kepegawaian`} component={lazy(() => import(`./master/kepegawaian`))} />
+      <Route path={`${match.url}/kepegawaian-add`} component={lazy(() => import(`./master/kepegawaian/add`))} />
+      <Route path={`${match.url}/kepegawaian-edit/:id`} component={lazy(() => import(`./master/kepegawaian/edit`))} />
       <Redirect from={`${match.url}`} to={`${match.url}/identitas`} />
     </Switch>
   </Suspense>
