870223d68994a2cacd643031254ce0fbd3d66817
nurd0tid <mnur29054@gmail.com>
2022-06-25 14:55:07 +0700

    Add Comment in Routes API For Detail Function Use

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const path = require('path')
 const bodyParser = require('body-parser');
 const identityRoutes = require('./utils/routes/master/identityRoutes');
+
+// API Master
 const identitasRoutes = require('./utils/routes/master/identitasRoutes');
 const kurikulumRoutes = require('./utils/routes/master/kurikulumRoutes');
 const akademikRoutes = require('./utils/routes/master/akademikRoutes');
@@ -12,8 +14,9 @@ const ptkRoutes = require('./utils/routes/master/ptkRoutes');
 const jurusanRoutes = require('./utils/routes/master/jurusanRoutes');
 const kelasRoutes = require('./utils/routes/master/kelasRoutes');
 const kepegawaianRoutes = require('./utils/routes/master/kepegawaianRoutes');
+// API Pengguna
 const guruRoutes = require('./utils/routes/pengguna/guruRoutes');
-
+// API Akademik
 const kmapelRoutes = require('./utils/routes/akademik/kmapelRoutes');
 const mapelRoutes = require('./utils/routes/akademik/mapelRoutes');
 
@@ -25,9 +28,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.set('views',path.join(__dirname,'views'));
 app.use('/img',express.static(__dirname + '/public/img'));
-
- 
-// set api routing
+// API Master
 app.use('/api/identity', identityRoutes);
 app.use('/api/identitas', identitasRoutes);
 app.use('/api/kurikulum', kurikulumRoutes);
@@ -39,9 +40,9 @@ app.use('/api/ptk', ptkRoutes);
 app.use('/api/jurusan', jurusanRoutes);
 app.use('/api/kelas', kelasRoutes);
 app.use('/api/kepegawaian', kepegawaianRoutes);
+// API Pengguna
 app.use('/api/guru', guruRoutes);
-
-
+// API Akademik
 app.use('/api/kmapel', kmapelRoutes);
 app.use('/api/mapel', mapelRoutes);
 
