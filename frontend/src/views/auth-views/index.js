5f89f6ab9de63fbe790db3580793dfef4be80ef3
nurd0tid <mnur29054@gmail.com>
2022-06-27 10:04:22 +0700

    Review Code

--- frontend/src/views/auth-views/index.js
+++ frontend/src/views/auth-views/index.js
@@ -11,7 +11,6 @@ export const AppViews = () => {
         <Route path={`${AUTH_PREFIX_PATH}/register`} component={lazy(() => import(`./authentication/register`))} />
         <Route path={`${AUTH_PREFIX_PATH}/forgot-password`} component={lazy(() => import(`./authentication/forgot-password`))} />
         <Route path={`${AUTH_PREFIX_PATH}/error-1`} component={lazy(() => import(`./errors/error-page-1`))} />
-        <Route path={`${AUTH_PREFIX_PATH}/error-2`} component={lazy(() => import(`./errors/error-page-2`))} />
         <Redirect from={`${AUTH_PREFIX_PATH}`} to={`${AUTH_PREFIX_PATH}/login`} />
       </Switch>
     </Suspense>
