6c32ae535d1268dc100fd53b49703b5943e0f36e
nurd0tid <mnur29054@gmail.com>
2022-07-14 22:38:47 +0700

    reduc again

--- frontend/src/redux/reducers/Auth.js
+++ frontend/src/redux/reducers/Auth.js
@@ -1,20 +1,47 @@
 import {
-	AUTH_TOKEN,
-	AUTHENTICATED,
-	SHOW_AUTH_MESSAGE,
-	HIDE_AUTH_MESSAGE,
-	SIGNOUT_SUCCESS,
-	SIGNUP_SUCCESS,
-	SHOW_LOADING,
+	// AUTH_TOKEN,
+	// AUTHENTICATED,
+	// SHOW_AUTH_MESSAGE,
+	// HIDE_AUTH_MESSAGE,
+	// SIGNOUT_SUCCESS,
+	// SIGNUP_SUCCESS,
+	// SHOW_LOADING,
+	AUTHENTICATED, 
+	NOT_AUTHENTICATED
 } from '../constants/Auth';
 
-const initState = {
-  loading: false,
-  message: '',
-  showMessage: false,
-  redirect: '',
-  accessToken: localStorage.getItem(AUTH_TOKEN),
-}
+// const initState = {
+//   loading: false,
+//   message: '',
+//   showMessage: false,
+//   redirect: '',
+//   accessToken: localStorage.getItem(AUTH_TOKEN),
+// }
+
+const initialState = {
+  authChecked: false,
+  loggedIn: false,
+  currentUser: {}
+};
+
+// export default function authReducer(state = initialState, action) {
+//   switch (action.type) {
+//     case AUTHENTICATED:
+//       return {
+//         authChecked: true,
+//         loggedIn: true,
+//         currentUser: action.payload,
+//       };
+//     case NOT_AUTHENTICATED:
+//       return {
+//         authChecked: true,
+//         loggedIn: false,
+//         currentUser: {}
+//       };
+//     default:
+//       return state;
+//   }
+// }
 
 const auth = (state = initState, action) => {
 	switch (action.type) {
